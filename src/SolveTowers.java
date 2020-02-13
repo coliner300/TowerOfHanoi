b1975710727673ab5f0d1c86df1d5a45f93f9e62
91085005 <coliner300@gmail.com>
2020-02-13 13:43:27 -0800

    Done

---
+++ src/SolveTowers.java
@@ -0,0 +1,9 @@
+public class SolveTowers
+{
+    public static void main(String[] args)
+    {
+        TowersOgHanoi towers = new TowersOgHanoi(4);
+        towers.solve();
+    }
+}
+//Christopher Oliner
---
+++ src/TowersOgHanoi.java
@@ -0,0 +1,30 @@
+public class TowersOgHanoi {
+    private int totalDisks;
+    public TowersOgHanoi(int disks)
+    {
+        totalDisks = disks;
+    }
+
+    public void solve()
+    {
+        moveTower(totalDisks, 1, 3, 2);
+    }
+
+    private void moveTower(int numDisk, int start, int end, int temp)
+    {
+        if (numDisk == 1)
+        {
+            moveOneDisk(start, end);
+        } else {
+            moveTower(numDisk - 1, start, temp, end);
+            moveOneDisk(start, end);
+            moveTower(numDisk - 1, temp, end, start);
+        }
+    }
+
+    private void moveOneDisk(int start, int end)
+    {
+        System.out.println("Move one disk from " + start + " to " + end);
+    }
+
+}
